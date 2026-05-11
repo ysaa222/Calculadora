1ff8db65f6207d27bb0be16c50b3a919179d835a
Isabelly <i.ramos@edu.senai.br>
2026-05-11 09:08:10 -0300

    Remocao Nome e Data do arquivo

--- calculdora.py
+++ calculdora.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Meu Primeiro Programa em Python
-Autor: [Seu Nome]
-Data: [Data de Hoje]
-Descrição: Programa básico para aprender GitHub
-"""
+
 
 # Importações
 import datetime
